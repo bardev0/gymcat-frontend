0343f8ad178c4e518f6da4a0889b7bd43e1ed68e
GregD <test@gmail.com>
2023-06-01 14:30:32 +0200

    feat: working login auth with encrypted passwords

--- src/components/LoginBox.tsx
+++ src/components/LoginBox.tsx
@@ -1,4 +1,39 @@
+import { useState } from "react";
+import bcryptjs from "bcryptjs";
+
+
 function LoginBox() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+	const [loginError, setLoginError] = useState("")
+
+  const handleName = (e: any) => setUsername(e.target.value);
+  const handlePassw = (e: any) => setPassword(e.target.value);
+
+	let data = {
+		name: username,
+		pass: password
+	}
+	
+	// pass
+	//
+  console.log(username + " " + password);
+
+  const sendData = async () => {
+		// let satlR = 13
+		// let salt = await bcryptjs.genSalt(satlR)
+		// let hash = await bcryptjs.hash(password, salt)
+		// Object.assign(data, {pass: hash})
+		// console.log(data)
+    const resposne = await fetch("http://localhost:5001/login", {
+      method: "POST",
+      mode: "cors",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(data),
+    });
+    resposne.json().then( (o) => {console.log(o) ; setLoginError(o.loginStatus)})
+  };
+
   return (
     <>
       <div>
@@ -6,14 +41,15 @@ function LoginBox() {
         <div>
           <form>
             <label className="loginLabel">username :</label>
-            <input type="text"></input>
+            <input onChange={handleName} type="text"></input>
           </form>
           <form>
             <label className="loginLabel">password :</label>
-            <input type="text"></input>
+            <input onChange={handlePassw} type="text"></input>
           </form>
-          <button>Login</button>
+          <button onClick={sendData}>Login</button>
         </div>
+				<div>{loginError}</div>
       </div>
     </>
   );
--- src/components/RegisterBox.tsx
+++ src/components/RegisterBox.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import * as bcryptjs from "bcryptjs";
 
 function RegisterBox() {
   const [username, setUsername] = useState("");
@@ -6,14 +7,30 @@ function RegisterBox() {
 
   const handleName = (e: any) => setUsername(e.target.value);
   const handlePassw = (e: any) => setPassword(e.target.value);
-	
-    console.log(username + " " + password);
 
-	const sendData = async () => {
-		let data = { name: username, pass: password }
-		console.log(data)
-		const resposne = await fetch('http://localhost:5001/addUser', {method: "POST",mode: "cors", headers: {"Content-Type": 'application/json'}, body: JSON.stringify(data)})
-		console.log(resposne)
+  console.log(username + " " + password);
+
+  const sendData = async () => {
+    let saltR = 13;
+    let data = { name: username };
+
+    let salt = await bcryptjs.genSalt(saltR);
+    let hash = await bcryptjs.hash(password, salt);
+    //**
+    // bcryptjs.genSalt(saltR, (salt) =>
+    // bcryptjs.hash(password, salt, (hash) =>
+    // Object.assign(data, { password: hash })
+    // )
+    // );
+    Object.assign(data, { password: hash });
+    console.log(data);
+    const resposne = await fetch("http://localhost:5001/addUser", {
+      method: "POST",
+      mode: "cors",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(data),
+    });
+    console.log(resposne);
   };
 
   return (
