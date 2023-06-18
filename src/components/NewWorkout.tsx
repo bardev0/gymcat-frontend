c220b5a09ae48464517613179f963d953857ada9
GregD <test@gmail.com>
2023-06-18 17:43:15 +0200

    working f<>b connection

--- src/components/NewWorkout.tsx
+++ src/components/NewWorkout.tsx
@@ -1,13 +1,38 @@
 import Table from "./Table";
 import useStore from "../Store";
+import { useEffect, useState } from "react";
 function NewWorkout() {
   // const storeArry = useStore((state: any) => state.temporaryWorkout);
   // const [table, setTable] = useState({});
+
+  const [value, setValue] = useState(useStore.getState().startingTemplate);
+
+  // redo for learning
+  useEffect(() => {
+    const unsubscribe = useStore.subscribe(
+      (newValue) => {
+        setValue(newValue);
+      },
+      (state) => state.value
+    );
+
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
+  // remove hardcode endpiont
+	//
+  const handleClick = async () => {
+    let respons = await fetch("http://localhost:5001/addWorkout", {
+			headers: {'Content-Type': 'application/json'},
+      method: "POST",
+      mode: "cors",
+      body: JSON.stringify({"work": value.startingTemplate}),
+    }) 
+		}
 	
-  const handleClick = () => {
-  	console.log("saving ...")
-		console.log(grab)}
-	;
+
   return (
     <>
       <Table></Table>
